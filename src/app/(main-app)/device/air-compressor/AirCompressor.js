cad1ab871aa0260bef39b992f58afb98db21394b
Wachirawit Klankla <wachirawitklankla@MacBook-Pro-khxng-Wachirawit.local>
2025-03-10 10:11:45 +0700

    fix path file

--- src/app/(main-app)/device/air-compressor/AirCompressor.js
+++ src/app/(main-app)/device/air-compressor/AirCompressor.js
@@ -3,16 +3,7 @@ import React from "react";
 // import SummaryCard from "./components/SummaryCard";
 
 import dynamic from "next/dynamic";
-// import DeviceControl from "./components/Control";
-
-// import Header1 from "./components/Header";
-
-
-
-// const SummaryCard = dynamic(() => import("./components/SummaryCard"), {
-//   ssr: false,
-// });
-const Header1 = dynamic(() => import("./components/Header"), {
+const SummaryCard = dynamic(() => import("./components/SummaryCard"), {
   ssr: false,
 });
 
@@ -20,8 +11,7 @@ export default function AirCompressor() {
   return (
     <div className="min-h-screen flex w-full text-enzy-dark dark:text-slate-200">
       <main className="p-4 lg:p-8 flex flex-1 flex-col bg-[#EDF2F8] dark:bg-dark-base">
-        <Header1 />
-       
+        <SummaryCard />
       </main>
     </div>
   );
